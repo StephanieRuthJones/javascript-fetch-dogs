c8b38eab1334503b757541a2bcce6d829e1817ca
Stephanie Jones <stephanie@Stephanies-MacBook-Pro.local>
2019-12-13 12:03:38 -0700

    added classname

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ function createDogs(dogs) {
 function createDogCard(dog) {
     const dogsContainer = document.querySelector('.dogs-container')
     const dogInfo = document.createElement('div')
-    dogInfo.classList.add('dog-info')
+    dogInfo.className = 'dog-info'
     dogInfo.innerHTML = `
             <img src=${dog.image} alt=${dog.id}/>
-            <h1>${dog.name}</h1>
-            <p>${dog.breed}</p>
-            <p>${dog.age}</p>
+            <h1>Name: ${dog.name}</h1>
+            <p>Breed: ${dog.breed}</p>
+            <p>Age: ${dog.age}</p>
         `
     dogsContainer.appendChild(dogInfo)
+    console.log(dogInfo)
 }
 
